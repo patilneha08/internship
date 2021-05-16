9d44867021083fbea77089035603e7d2ca86b54c
Mark Wagh <mark.wagh20@gmail.com>
2021-05-16 21:10:59 +0530

    Added Event Manager login and Registration

--- eventmanagement/urls.py
+++ eventmanagement/urls.py
@@ -5,5 +5,8 @@ urlpatterns=[
     path('',views.index,name="index"),
     path("userlogin",views.userlogin,name="userlogin"),
     path("userlogout",views.userlogout,name="userlogout"),
-    path("userregister",views.userregister,name="userregister")
+    path("userregister",views.userregister,name="userregister"),
+    path("eventmanagerlogin",views.eventmanagerlogin,name="eventmanagerlogin"),
+    path("eventmanagerregister",views.eventmanagerregister,name="eventmanagerregister"),
+    path("eventmanagerlogout",views.eventmanagerlogout,name="eventmanagerlogout"),
 ]
