96ea5be54451fa28aa634645322e9cc724d1b8c0
patilneha08 <patil.neha08@yahoo.com>
2021-05-17 00:32:08 +0530

    User login authentication

--- eventmanagement/admin.py
+++ eventmanagement/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import Eventmanager, CustomUser
 
-admin.site.register(User)
-
+admin.site.register(CustomUser)
+admin.site.register(Eventmanager)
