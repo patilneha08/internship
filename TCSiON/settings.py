a92f0e373248cac41168959a09cb113d24ed3ab9
patilneha08 <patil.neha08@yahoo.com>
2021-05-26 13:53:38 +0530

    final commit

--- TCSiON/settings.py
+++ TCSiON/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kcp=#^0*cq)+9y&=5k#h@1gmo+xxkcv4h6(!va)=2-u!e63q8w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -42,7 +41,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
